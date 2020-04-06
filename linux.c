551d02308d0c9b6e4dafbd9eacee015386c64276
dspangpang <873727286@qq.com>
2020-04-06 20:59:51 +0800

    只写到无名管道   2020 4/6

---
+++ linux.c
@@ -0,0 +1,50 @@
+#include <fcntl.h> 
+#include <unistd.h>
+//进程间通信：
+
+/****************************创建子进程************************************************************************/
+pid_t pid_err;
+pid_t pid_err = fork();
+if(pid_err<0)
+	printf("create process failur\n")            //返回值 小于0 创建失败      如有需要可以 return 结束进程
+
+if(pid_err>0){
+	printf("This is father process \n")		    //返回值大于0 即返回值为 子进程的 pid号  为父进程
+}
+
+if(pid_err==0){
+    printf("This is child process \n")				//返回值等于0，即为子进程  通过 pid的不同 执行不同进程的不同命令
+}
+
+
+/******************************无名管道的用法************************************************************************/
+/*单独构成一种独立的文件系统：管道对于管道两端的进程而言，就是一个文件，但它不是普通的文件，它不属于某种文件系统，而是自立门户，
+单独构成一种文件系统，并且只存在与内存中。*/
+
+
+//管道是半双工的 ，一管道只能  一边进一边出。要想实现两个进程间的像互通信  需要两个管道;  类似于队列
+
+int pipe(int pipefd[2]);  //pipe 函数
+int ret;
+//使用方法            先创建管道 再创建子进程
+
+int pipefd[2];       //pipe是一种特殊的文件   就需要创建他的文件描述符
+
+ret = pipe(pipefd);     //成功返回0 失败返回 -1，需要判断是否创建成功；
+
+if(ret<0)
+	printf("create pipe failure\n");     //注意 printf 为行缓存 ,如果创建失败 ，可以 return 结束进程
+
+char write_buffer[] = "hello linux";
+char read_buffer[128] = {0};               // 注意 如果想要给字符串赋值  只能使用  memset   和  memcpy  的  字符串操作函数
+
+write(fd[1],write_buffer,sizeof(write_buffer));
+read(fd[0],read_buffer,128);                       //向fd[1]中写入数据 ，从 fd[0]中读出数据；读出相当于出队 数据消失
+
+
+
+/*管道提供了简单的流控制机制，进程试图读空管道时，进程将阻塞。同样，管道已经满时，进程再试图向管道写入数据，进程将阻塞*/
+
+
+
+
